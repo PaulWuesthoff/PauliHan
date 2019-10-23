082385a7e8457141d24e9f24bbe949b9e36f6fda
AlihanAt <alihan_a98@hotmail.de>
2019-10-23 12:53:35 +0200

    ordnerstruktur

---
+++ testrunner/src/main/java/htwb/ai/PauliHan/MyTest.java
@@ -0,0 +1,14 @@
+package htwb.ai.PauliHan;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Documented
+public @interface MyTest {
+	String testString();
+}
---
+++ testrunner/src/main/java/htwb/ai/PauliHan/MyTestExample.java
@@ -0,0 +1,11 @@
+package htwb.ai.PauliHan;
+
+public class MyTestExample {
+
+	@MyTest(
+			testString="Hallo, Test.")
+	public void bugFix() {
+		
+	}
+
+}
