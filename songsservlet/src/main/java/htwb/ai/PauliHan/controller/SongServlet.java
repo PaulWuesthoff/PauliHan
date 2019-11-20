6852a52290ba0b14578eb91882c7592cc645cf13
PaulWuesthoff <56341847+PaulWuesthoff@users.noreply.github.com>
2019-11-20 13:41:46 +0100

    -created multiple Classes
    - song class created
    - ReadJson class created

---
+++ songsservlet/src/main/java/htwb/ai/PauliHan/controller/SongServlet.java
@@ -0,0 +1,25 @@
+package htwb.ai.PauliHan.controller;
+
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class SongServlet {
+
+    private String gitVersion;
+
+
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+    }
+}
---
+++ songsservlet/src/main/java/model/ReadJson.java
@@ -0,0 +1,35 @@
+package model;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import java.io.*;
+
+import java.util.List;
+
+public class ReadJson {
+//
+//    public static void main(String[] args) {
+//        try {
+//            List<Song> readSongs = readJSONToSongs("src/main/resources/songs.json");
+//            readSongs.forEach(s -> {
+//                if(s.getId() == 1)
+//                System.out.println(s.toString());
+//            });´
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+    public static List<Song> readJSONToSongs(String filename) throws FileNotFoundException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try (InputStream is = new BufferedInputStream(new FileInputStream(filename))) {
+            return (List<Song>) objectMapper.readValue(is, new TypeReference<List<Song>>() {
+            });
+        }
+    }
+
+}
