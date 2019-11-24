67c2566c5691684fe1b1b1d14fa44a1a2dbda703
AlihanAt <alihan_a98@hotmail.de>
2019-11-24 16:17:12 +0100

    beautify, fehler: speichert nicht mehr nach destroy

--- songsservlet/src/main/java/htwb/ai/PauliHan/controller/SongServlet.java
+++ songsservlet/src/main/java/htwb/ai/PauliHan/controller/SongServlet.java
@@ -25,103 +25,93 @@ import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/songsservlet-PauliHan", name = "songservlet")
 public class SongServlet extends HttpServlet {
-    private JsonReader reader;
-    private Database database;
-    private Gson gson;
-    private PrintWriter out;
+	private JsonReader reader;
+	private Database database;
+	private Gson gson;
+	private PrintWriter out;
 
+	public void init(ServletConfig servletConfig) throws ServletException {
+		reader = new JsonReader();
+		try {
 
+			database = new Database(reader.readJSONToSongs("songs.json"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void init(ServletConfig servletConfig) throws ServletException {
-        reader = new JsonReader();
-        try {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		out = response.getWriter();
 
-            database = new Database(reader.readJSONToSongs("songs.json"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		if (checkIfParamterIsSongId(request)) {
+			String parameter = request.getParameter("songId");
+			int songId = Integer.parseInt(parameter);
 
+			if (checkIfValueIsInRange(songId)) {
+				switch (checkHttpHeader(request)) {
+				case "json": {
+					doGetHeaderJson(response, songId);
+					break;
+				}
+				case "xml": {
+					JAXBContext context = null;
+					try {
+						context = JAXBContext.newInstance(Song.class);
+					} catch (JAXBException e) {
+						response.setStatus(400);
+						e.printStackTrace();
+					}
+					Marshaller marshaller = null;
+					try {
+						marshaller = context.createMarshaller();
+					} catch (JAXBException e) {
+						response.setStatus(400);
+						e.printStackTrace();
+					}
+					try {
+						marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+					} catch (PropertyException e) {
+						response.setStatus(400);
+						e.printStackTrace();
+					}
+					Song song = database.getSongFromMapById(songId);
+					try {
+						marshaller.marshal(song, out);
+					} catch (JAXBException e) {
+						response.setStatus(400);
+						e.printStackTrace();
+					}
+					out.flush();
+					break;
+				}
+				case "*/*": {
+					doGetHeaderJson(response, songId);
+					break;
+				}
+				case "empty": {
+					doGetHeaderJson(response, songId);
+					break;
+				}
+				case "null": {
+					doGetHeaderJson(response, songId);
 
-
-    
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        out = response.getWriter();
-
-        if (checkIfParamterIsSongId(request)) {
-            String parameter = request.getParameter("songId");
-            int songId = Integer.parseInt(parameter);
-            
-            if (checkIfValueIsInRange(songId)) {
-                switch (checkHttpHeader(request)) {
-                    case "json": {
-                    	doGetHeaderJson(response, songId);
-                        break;
-                    }
-                    case "xml": {
-                        JAXBContext context = null;
-                        try {
-                            context = JAXBContext.newInstance(Song.class);
-                        } catch (JAXBException e) {
-                            response.setStatus(400);
-                            e.printStackTrace();
-                        }
-                        Marshaller marshaller = null;
-                        try {
-                            marshaller = context.createMarshaller();
-                        } catch (JAXBException e) {
-                            response.setStatus(400);
-                            e.printStackTrace();
-                        }
-                        try {
-                            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-                        } catch (PropertyException e) {
-                            response.setStatus(400);
-                            e.printStackTrace();
-                        }
-                        Song song = database.getSongFromMapById(songId);
-                        try {
-                            marshaller.marshal(song, out);
-                        } catch (JAXBException e) {
-                            response.setStatus(400);
-                            e.printStackTrace();
-                        }
-                        out.flush();
-                        break;
-                    }
-                    case "*/*": {
-                        gson = new Gson();
-                        out.write(gson.toJson(database.getSongFromMapById(songId)));
-                        out.flush();
-                        break;
-                    }
-                    case "empty": {
-                        gson = new Gson();
-                        out.write(gson.toJson(database.getSongFromMapById(songId)));
-                        out.flush();
-                        break;
-                    }
-                    case "null": {
-                        gson = new Gson();
-                        out.write(gson.toJson(database.getSongFromMapById(songId)));
-                        out.flush();
-                        break;
-                    }
-                    case "error": {
-                        response.setStatus(406);
-                        break;
-                    }
-                    default: {
-                        response.setStatus(400);
-                    }
-                }
-            } else {
-                response.setStatus(404);
-            }
-        } else {
-            response.setStatus(400);
-        }
-        // range zwischen 1 und 10
+					break;
+				}
+				case "error": {
+					response.setStatus(406);
+					break;
+				}
+				default: {
+					response.setStatus(400);
+				}
+				}
+			} else {
+				response.setStatus(404);
+			}
+		} else {
+			response.setStatus(400);
+		}
+		// range zwischen 1 und 10
 //        PrintWriter out = response.getWriter();
 //        if (!request.getHeader("Accept").equals("application.json")
 //                || !request.getHeader("Accept").equals("application.xml"))
@@ -163,115 +153,107 @@ public class SongServlet extends HttpServlet {
 //            response.setStatus(400);
 //        }
 
-        // PrintWriter out = response.getWriter();
-        // out.println(songList.get(songId - 1).toString());
-        // der Request gibt mir alle infos mit
-        // anhand der request ein song zurückgeben
-    }
+		// PrintWriter out = response.getWriter();
+		// out.println(songList.get(songId - 1).toString());
+		// der Request gibt mir alle infos mit
+		// anhand der request ein song zurückgeben
+	}
 
-    
+	public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		out = response.getWriter();
+		if (checkIfParamterIsSongId(request)) {
+			String parameter = request.getParameter("songId");
+			int songId = Integer.parseInt(parameter);
 
-    
+			if (checkIfValueIsInRange(songId)) {
+				switch (checkHttpHeaderContentType(request)) {
 
-    public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        out = response.getWriter();
-        if (checkIfParamterIsSongId(request)) {
-            String parameter = request.getParameter("songId");
-            int songId = Integer.parseInt(parameter);
-            
-            if (checkIfValueIsInRange(songId)) {
-                switch (checkHttpHeaderContentType(request)) {
+				case "json":
+					doPutHeaderJson(request, response, songId);
+					break;
 
-                    case "json":
-                    	doPutHeaderJson(request, response, songId);
-                        break;
+				default:
+					response.setStatus(400);
+					break;
+				}
+			}
+		}
+	}
 
-                    default:
-                        response.setStatus(400);
-                        break;
-                }
-            }
-        }
-    }
+	public void destroy() {
+		JsonWriter writer = new JsonWriter();
+		writer.writeSongsToJson(database.getSongMap());
+	}
 
-    public void destroy() {    
-        JsonWriter writer = new JsonWriter();
-        writer.writeSongsToJson(database.getSongMap());
-    }
-    
-    public void doGetHeaderJson(HttpServletResponse response, int songId) throws IOException {
-    	out = response.getWriter();
-    	gson = new Gson();
-        out.write(gson.toJson(database.getSongFromMapById(songId)));
-        out.flush();
-    }
-    
-    public void doPutHeaderJson(HttpServletRequest request, HttpServletResponse response, int songId) throws IOException {
-        BufferedReader in = request.getReader();
-        ObjectMapper objectMapper = new ObjectMapper();
-        Song updatedSong = objectMapper.readValue(in, Song.class);
-        
-        if (updatedSong.getId() == songId) {
-            if (database.update(songId, updatedSong)) {
-                response.setStatus(204);
+	public void doGetHeaderJson(HttpServletResponse response, int songId) throws IOException {
+		out = response.getWriter();
+		gson = new Gson();
+		out.write(gson.toJson(database.getSongFromMapById(songId)));
+		out.flush();
+	}
 
-            } else {
-                response.setStatus(400);
-            }
-        } else {
-            response.setStatus(400);
-        }
-    }
-        
-    public String checkHttpHeaderContentType(HttpServletRequest request) {
-        if (request.getHeader("Content-Type").contains("application/json")) {
-            return "json";
-        }
-        return "error";
-    }
+	public void doPutHeaderJson(HttpServletRequest request, HttpServletResponse response, int songId) throws IOException {
+		BufferedReader in = request.getReader();
+		ObjectMapper objectMapper = new ObjectMapper();
+		Song updatedSong = objectMapper.readValue(in, Song.class);
 
-    public String checkHttpHeader(HttpServletRequest request) {
-        if (request.getHeader("Accept").contains("application/json")) {
-            return "json";
+		if (updatedSong.getId() == songId) {
+			if (database.update(songId, updatedSong)) {
+				response.setStatus(204);
 
-        }
-        if (request.getHeader("Accept").contains("application/xml")) {
-            return "xml";
+			} else {
+				response.setStatus(400);
+			}
+		} else {
+			response.setStatus(400);
+		}
+	}
 
-        }
-        if (request.getHeader("Accept").contains("*/*")) {
-            return "*/*";
-        }
-        if (request.getHeader("Accept").isEmpty()) {
-            return "empty";
-        }
-        if (request.getHeader("Accept").equals(null)) {
-            return "null";
-        }
-        return "error";
-    }
-    
-    public boolean checkIfValueIsInRange(int req) {		
-        if (req < 1 || req > 10)
-            return false;
-        else
-            return true;
-    }
-    
-    public boolean checkIfParamterIsSongId(HttpServletRequest request) {
-    	if(request.getParameterMap().containsKey("songId")) {
-    		return true;
-    	}
-    	return false;
-    }
+	public String checkHttpHeaderContentType(HttpServletRequest request) {
+		if (request.getHeader("Content-Type").contains("application/json")) {
+			return "json";
+		}
+		return "error";
+	}
 
-    public boolean checkIfParameterContainsString(String parameter) {	//wird gar nicht benutzt
-        if (parameter.contains("songId"))
-            return true;
-        else
-            return false;
-    }
-    
+	public String checkHttpHeader(HttpServletRequest request) {
+		if (request.getHeader("Accept").contains("application/json")) {
+			return "json";
+		}
+		if (request.getHeader("Accept").contains("application/xml")) {
+			return "xml";
+		}
+		if (request.getHeader("Accept").contains("*/*")) {
+			return "*/*";
+		}
+		if (request.getHeader("Accept").isEmpty()) {
+			return "empty";
+		}
+		if (request.getHeader("Accept").equals(null)) {
+			return "null";
+		}
+		return "error";
+	}
 
+	public boolean checkIfValueIsInRange(int req) {
+		if (req < 1 || req > 10)
+			return false;
+		else
+			return true;
+	}
+
+	public boolean checkIfParamterIsSongId(HttpServletRequest request) {
+		if (request.getParameterMap().containsKey("songId")) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean checkIfParameterContainsString(String parameter) { // wird gar nicht benutzt
+		if (parameter.contains("songId"))
+			return true;
+		else
+			return false;
+	}
 
 }
