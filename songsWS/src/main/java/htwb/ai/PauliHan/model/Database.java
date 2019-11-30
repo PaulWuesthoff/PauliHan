1ba89be1f701542d911bedde92f609bc817ba315
PaulWuesthoff <56341847+PaulWuesthoff@users.noreply.github.com>
2019-11-30 11:43:55 +0100

    - Database

--- songsWS/src/main/java/htwb/ai/PauliHan/model/Database.java
+++ songsWS/src/main/java/htwb/ai/PauliHan/model/Database.java
@@ -1,4 +1,42 @@
 package htwb.ai.PauliHan.model;
 
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Database {
+    private ConcurrentHashMap<Integer, Song> songMap;
+
+    public Database(List<Song> songsList) {
+        this.songMap = new ConcurrentHashMap<>();
+        songsList.forEach(song -> this.songMap.put(song.getId(), song));
+
+    }
+
+    public Song getSongFromMapById(int id) {
+        return songMap.get(id);
+    }
+
+    public boolean update(int id, Song update) {
+        //hier für put ids aktualisieren
+
+        boolean updated = true;
+        try {
+            songMap.get(id).setTitle(update.getTitle());
+            songMap.get(id).setArtist(update.getArtist());
+            songMap.get(id).setLabel(update.getLabel());
+            songMap.get(id).setReleased(update.getReleased());
+        } catch (Exception e) {
+            updated = false;
+        }
+        return updated;
+    }
+
+    public int getSize() {
+        return songMap.size();
+    }
+
+    public ConcurrentHashMap<Integer, Song> getSongMap() {
+        return songMap;
+    }
+
 }
--- songsWS/src/main/java/htwb/ai/PauliHan/model/XMLWriter.java
+++ songsWS/src/main/java/htwb/ai/PauliHan/model/XMLWriter.java
@@ -1,4 +1,28 @@
 package htwb.ai.PauliHan.model;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.PrintWriter;
+import java.util.Map;
+
 public class XMLWriter {
+
+
+    public boolean writeSongsToXML(Database database, PrintWriter out){
+        try {
+            JAXBContext context = JAXBContext.newInstance(Song.class);
+            Marshaller marshaller = null;
+            marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            Map<Integer,Song> song = database.getSongMap();
+            marshaller.marshal(song, out);
+            return true;
+        } catch (JAXBException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
 }
